60de81ff6322a2096d0d5efbdeb285cd9ecec94a
unknown <mihail.tilica@liceulvarlaam.ro>
2022-01-23 10:49:56 +0200

    todo

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+        <h1>Lista de cumparaturi</h1>
+    </div>
+  );
+}
+
+export default Header;
+
